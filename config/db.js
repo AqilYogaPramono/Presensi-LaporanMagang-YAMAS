d1e0132129f3deeb698239af1c1ac1276c4f9756
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-08-15 00:47:22 +0700

    feat: move to mysql2

--- config/db.js
+++ config/db.js
@@ -1,15 +1,13 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
-database = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-})
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}).promise()
 
-database.connect((err) => {
-    if(err) return console.log(err)
-    return console.log("Berhasil Terhubung")
-})
-
-module.exports = database
+module.exports = pool
